f33b71a777b5a10a0dc30a9ced749375a6bbfffe
shivamanhar <shivamanhar@gmail.com>
2015-03-16 07:56:07 +0530

    Revert "Adding registration functionality"
    This reverts commit e9916c07237a6530b59aa2ed136f1ee687fb6e78.

--- application/modules/login/controllers/register.php
+++ application/modules/login/controllers/register.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class register extends MX_Controller
+class register extends CI_Controller
 {
 
     /**
@@ -42,12 +42,6 @@ class register extends MX_Controller
         //Load helpers
         $this->load->helper(array('form', 'url'));
         
-        //Load database class
-        $this->load->database();
-        
-        //Load model
-        $this->load->model("usermodel");
-        
         //Validation rules
         $this->form_validation->set_rules("Username", "Username", "trim|min_length[5]|required");
         $this->form_validation->set_rules("Password", "Password", "trim|min_length[5]|required");
@@ -55,42 +49,14 @@ class register extends MX_Controller
         
         if ( $this->form_validation->run() == TRUE ) {
             
-            //Get user details from post array.
-            $username = $this->input->post("Username");
-            $password = $this->input->post("Password");
-            $email = $this->input->post("Email");
-            
-            //Save in array for use in function later.
-            $userDetails = array("username" => $username,
-                                "password" => $password,
-                                "email" => $email);
-            
             //Check to see if username is unique
-            $query = "SELECT Username from user where Username = ?";
-            $queryResult = $this->db->query( $query, array($username) ) ;
-            $queryResult =  $queryResult->num_rows();
             
+            //Prepare password
             
-            if ($queryResult < 1) {
-                 
-                //Prepare password
-                $password = $this->usermodel->prepare_password($password);
-                $userDetails['password'] = $password;
-
-                //Insert data into database
-                
-                if ($this->usermodel->createUser($userDetails)) {
-                    echo "User account created";
-                }
-                else {
-                    echo "There was a problem creating the account. Please try again.";
-                }
-                
-
-                //Redirect user
-            }
-           
+            //Insert data into database
             
+            //Redirect user
+            echo "hi";
         }
         //Form input invalid
         else {
